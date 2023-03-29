dd0a308f56a4392749e241494cf77c4c29744c83
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-29 17:56:26 -0400

    Create ValidSudoku.py

---
+++ Python/Arrays&Hashing/ValidSudoku.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
