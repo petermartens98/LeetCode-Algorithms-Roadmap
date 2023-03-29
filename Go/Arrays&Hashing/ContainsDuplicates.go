3d9e5801f06b00d75b6815c52031d4a7c0b74030
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-29 14:57:43 -0400

    Create ContainsDuplicates.go

---
+++ Go/Arrays&Hashing/ContainsDuplicates.go
@@ -0,0 +1,10 @@
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+    for _, num := range nums {
+        if seen[num] {
+            return true
+        }
+        seen[num] = true
+    }
+    return false
+}
