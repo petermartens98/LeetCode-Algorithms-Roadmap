2f390a5ba9edbdc2f1647b3922f0b90f7bf76d46
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-29 22:13:51 -0400

    Create BinaryTreeLevelOrderTraversal.py

---
+++ Python/Trees/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        # Use BFS Algotithm
+        # Complexity: O(n)
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
