c5403f0fe67b0e0841cb195e1c97817ab1304980
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-30 12:03:01 -0400

    Update Search2DMatrix.py

--- Python/BinarySearch/Search2DMatrix.py
+++ Python/BinarySearch/Search2DMatrix.py
@@ -16,11 +16,48 @@ class Solution(object):
         return False
         '''
 
-        # Solution 2
+        # Solution 2 
+        '''
         allNums = []
-        for i in matrix:
-            allNums += i
-        allNums = sorted(allNums)
-        if target in allNums:
-            return True
+        for i in matrix: allNums += i
+        if target in allNums: return True
         return False
+        '''
+
+        # Solution 3
+        '''
+        for i in matrix:
+            if target in i: return True
+            if min(i)>target: break
+        return False
+        '''
+
+        # Solution 4 - Binary Search
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top+bot)//2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+                
+        if not (top <= bot):
+            return False
+        
+        row = (top+bot)//2
+        l, r = 0, COLS - 1
+        while l<=r:
+            m = (l+r)//2
+            if target > matrix[row][m]:
+                l = m+1
+            elif target < matrix[row][m]:
+                r = m-1
+            else:
+                return True
+        return False
+
+
+        
