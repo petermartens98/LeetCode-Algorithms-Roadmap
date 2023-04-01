c538ef45be3bdc5af28fb981725aba70fc665274
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-01 11:08:48 -0400

    Create PermutationInString.py

---
+++ Python/SlidingWindow/PermutationInString.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        # Solution 1 - Time Limit Exceeded
+        '''
+        for i, _ in enumerate(s2[:len(s2)-len(s1)+1]):
+            if sorted(s2[i:i+len(s1)]) == sorted(s1):
+                return True
+        return False
+        '''
+        # Solution 2
+        if len(s1) > len(s2): return False
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
