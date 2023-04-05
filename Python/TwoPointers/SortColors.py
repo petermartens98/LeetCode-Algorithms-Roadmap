8f41b150f00c46c4f73c7b52301bda0603070671
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-05 12:10:36 -0400

    Create SortColors.py

---
+++ Python/TwoPointers/SortColors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        r, w, b = 0, 0, len(nums)-1
+        while w <= b:
+            if nums[w] == 0:
+                nums[r], nums[w] = nums[w], nums[r]
+                w += 1
+                r += 1
+            elif nums[w] == 1: w += 1
+            else:
+                nums[w], nums[b] = nums[b], nums[w]
+                b -= 1
