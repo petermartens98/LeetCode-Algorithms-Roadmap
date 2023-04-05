684472da12051541f98365b2cd562e56e17e90aa
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-05 12:18:47 -0400

    Create FindDuplicate.py

---
+++ Python/Arrays&Hashing/FindDuplicate.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen: return num
+            seen.add(num)
