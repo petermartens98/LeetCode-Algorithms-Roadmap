24ef8b0178ed708918558c60222e7a7e8cd6eb2d
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-05-17 11:42:27 -0400

    Create KthSmallestElementBST.py

---
+++ Python/Trees/KthSmallestElementBST.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        n = 0
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
+
