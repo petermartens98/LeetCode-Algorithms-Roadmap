95f0d29e45b011771efe54efbcc21b407f0a25b8
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-05-22 13:45:14 -0400

    Create CombinationSum.py

---
+++ Python/BackTracking/CombinationSum.py
@@ -0,0 +1,52 @@
+# Solution 1
+'''
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+        dfs(0, [], 0)
+        return res
+'''
+# Solution 2
+'''
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()  
+        self.dfs(candidates, 0, [], target, res)
+        return res
+
+    def dfs(self, candidates, start, combination, target, res):
+        if target == 0:
+            res.append(combination)
+            return
+        for i in range(start, len(candidates)):
+            if candidates[i] > target: break
+            self.dfs(candidates, i, combination + [candidates[i]], target - candidates[i], res)
+'''
+# Solution 3
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        self.dfs(candidates, 0, [], target, res)
+        return res
+
+    def dfs(self, candidates, start, combination, target, res):
+        if target == 0:
+            res.append(combination)
+            return
+
+        for i in range(start, len(candidates)):
+            if candidates[i] > target:
+                break
+            self.dfs(candidates, i, combination + [candidates[i]], target - candidates[i], res)
