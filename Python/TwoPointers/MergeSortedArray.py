9c99082aaeef30b36ebf043b1b3b078587a1d3c9
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-05-26 10:14:09 -0400

    Create MergeSortedArray.py

---
+++ Python/TwoPointers/MergeSortedArray.py
@@ -0,0 +1,28 @@
+# Two Pointers Solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Solution 1
+        '''
+        last = m + n - 1
+        while m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m-=1
+            else:
+                nums1[last] = nums2[n-1]
+                n-=1
+            last-=1
+        while n>0:
+            nums1[last] = nums2[n-1]
+            n, last = n-1, last-1
+        '''
+        # Solution 2
+        last = m + n - 1
+        while m > 0 and n > 0:
+            nums1[last], m, n, last = (nums1[m-1], m-1, n, last) if nums1[m-1] > nums2[n-1] else (nums2[n-1], m, n-1, last)
+            last -= 1
+        while n > 0: nums1[last], n, last = nums2[n-1], n-1, last-1
+
